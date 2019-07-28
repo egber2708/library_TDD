ac478c48641950b4150b7717a89ee3e9e7f73805
egber2708 <egber27@hotmail.com>
2019-07-28 00:00:46 -0500

    book destroy test

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -17,6 +17,16 @@ class BooksController extends Controller
     }
 
 
+    public function destroy($book){
+        $book1 = Book::find($book);
+        $book1->delete();
+        // Generating Redirects...
+        return redirect('/');
+       
+    }
+
+
+
     protected function validateRequest(){
         return request()->validate([
             'title'=>"required",
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Route::post('/books', 'BooksController@store' );
 
 Route::patch('/books/{book}', 'BooksController@update' );
+
+Route::delete('/books/{book}', 'BooksController@destroy');
