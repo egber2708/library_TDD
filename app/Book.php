11808b8a73fd963ea074449ca8bd46764b004830
egber2708 <egber27@hotmail.com>
2019-07-27 22:51:46 -0500

    book created and update back_end

---
+++ app/Book.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    //turn off all the variable to the laravel...
+    protected $guarded = [];
+}
